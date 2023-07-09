b22efb1fb2b23cea9c1435564cf9a305966c97e8
Tomáš Bokor <tomas.bokor10@gmail.com>
2023-07-09 19:58:01 +0200

    mobile nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -12,6 +12,8 @@ const Nav = () => {
     const pathname = usePathname();
     const isCurrentlyHome = pathname === '/';
 
+    const [toggleDropdown, setToggleDropdown] = useState(false)
+
     return (
         <nav className="flex-between w-full mb-16 pt-3 px-5 fixed top-0 left-0">
             <Link href='/' className="flex gap-2 flex-center nav_logo_cont">
@@ -50,7 +52,33 @@ const Nav = () => {
                 }
             </div>
 
-        </nav>
+            <div type="button" className="flex sm:hidden">
+                <button className="cursor-pointer bg-gradient-to-r from-primary-green to-[green] drop-shadow-[0_0_5px_rgba(0,255,0,0.8)] rounded-sm w-8 h-8 hover:drop-shadow-[0_0_10px_rgba(0,255,0,1)] transition-all" onClick={() => setToggleDropdown((prev) => !prev)} ></button>
+
+                <div className={toggleDropdown ? 'dropdown_active dropdown' : 'dropdown'}>
+
+                    {isUserLoggedIn ? (
+                        <>
+                            {isCurrentlyHome ? (
+                                <Link href='/dashboard' className="dropdown_link" onClick={() => setToggleDropdown(false)}>Dashboard</Link>
+
+                            ) : (
+                                <Link href='/' className="dropdown_link" onClick={() => setToggleDropdown(false)}>Home</Link>
+                            )}
+
+                            <button type="button" onClick={signOut} className="dropdown_link">
+                                Sign Out
+                            </button>
+                        </>
+                    ) : (
+                        < Link href='/sign-in' className="dropdown_link" onClick={() => setToggleDropdown(false)}>Sign In</Link>
+                    )}
+
+                </div>
+
+            </div>
+
+        </nav >
     )
 }
 
